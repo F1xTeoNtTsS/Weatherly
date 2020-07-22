10bb58c0af1b9fa64f06da1cefb2caa385eba90e
dmitriiabanin <dmitriiabanin@yandex-team.ru>
2020-07-22 12:43:03 +0300

    done

--- Sunny/Models/NetworkWeatherManager.swift
+++ Sunny/Models/NetworkWeatherManager.swift
@@ -7,20 +7,64 @@
 //
 
 import Foundation
+import CoreLocation
 
-struct NetworkWeatherManager {
+class NetworkWeatherManager {
     
-    func fetchCurrentWeather(forCity city: String) {
-        
-        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=845760931e6add4ff7dbb28c975aa20c"
-        let url = URL(string: urlString)
+    enum RequestType {
+        case cityName(city: String)
+        case coordinate(latitude: CLLocationDegrees, longitude: CLLocationDegrees)
+    }
+    
+    var onCompletion: ((CurrentWeather) -> Void)?
+    
+    func fetchCurrentWeather(forRequestType requestType: RequestType) {
+        var urlString = ""
+        switch requestType {
+        case .cityName(let city):
+            urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&units=metric&appid=\(appid)"
+        case .coordinate(let latitude, let longitude):
+            urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&units=metric&appid=\(appid)"
+        }
+        performRequest(withUrlString: urlString)
+    }
+    
+//    func fetchCurrentWeather(forCity city: String) {
+//
+//        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&units=metric&appid=\(appid)"
+//        performRequest(withUrlString: urlString)
+//    }
+//
+//    func fetchCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+//
+//        let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&units=metric&appid=\(appid)"
+//        performRequest(withUrlString: urlString)
+//    }
+    
+    fileprivate func performRequest(withUrlString urlString: String) {
+        guard let url = URL(string: urlString) else { return }
         let session = URLSession(configuration: .default)
-        let task = session.dataTask(with: url!) { data, response, error in
+        let task = session.dataTask(with: url) { data, response, error in
             if let data = data {
-                let dataString = String(data: data, encoding: .utf8)
-                print(dataString!)
+                if let currentWeather = self.parseJSON(withData: data) {
+                    self.onCompletion?(currentWeather)
+                }
             }
         }
         task.resume()
+    }
+    
+    fileprivate func parseJSON(withData data: Data) -> CurrentWeather? {
+        let decoder = JSONDecoder()
+        do {
+            let currentWeatherData = try decoder.decode(CurrentWeatherData.self, from: data)
+            guard let currentWeather = CurrentWeather(currentWeatherData: currentWeatherData) else {
+                return nil
+            }
+            return currentWeather
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        return nil
     }
 }
---
+++ Sunny/Supporting files/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Sunny
+//
+//  Created by Dmitrii Abanin on 7/17/20.
+//  Copyright © 2020 Ivan Akulov. All rights reserved.
+//
+
+import Foundation
+
+let appid = "845760931e6add4ff7dbb28c975aa20c"
